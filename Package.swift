524b4ea41f86cdc657372edd79f83112792b5e91
Matthew Judy <matt@nibfile.com>
2023-12-04 21:28:54 -0500

    Bump `swift-argument-parser` to version `1.2.3`.

--- Package.swift
+++ Package.swift
@@ -37,5 +37,6 @@ let package: Package = Package(
         .executableTarget(name: "tuning-trouble",          dependencies: commonDependencies, path: "Sources/2022/06", swiftSettings: commonSettings),
         .executableTarget(name: "no-space-left-on-device", dependencies: commonDependencies, path: "Sources/2022/07", swiftSettings: commonSettings),
         .executableTarget(name: "treetop-tree-house",      dependencies: commonDependencies, path: "Sources/2022/08", swiftSettings: commonSettings),
+        .executableTarget(name: "trebuchet",               dependencies: commonDependencies, path: "Sources/2023/01", swiftSettings: commonSettings),
     ]
 )
